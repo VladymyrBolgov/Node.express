055761aaad214d4e22750d888a855d1db927e97f
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-12-05 00:58:47 +0200

    kl

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
+require('dotenv').config();
 
 //После мы подключаем роуты
 const indexRouter = require('./routes/index');
@@ -22,6 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 //Дальше устанавливаем обработку статических ресурсов
 app.use(express.static(path.join(__dirname, 'public')));
 
